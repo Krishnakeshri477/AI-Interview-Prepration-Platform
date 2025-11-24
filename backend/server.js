ce062bcb53e917ca908ab6d467445491f87c97cf
krishna_9_99_999 <krishnakeshri9510ib@gmail.com>
2025-11-24 23:38:28 +0530

    frontend public

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const connectDB = require('./src/config/db');
 const { errorHandler } = require('./src/middleware/errorHandler');
 const passport = require('passport');
 require('./src/config/passport');
+const path = require('path');
 
 const app = express();
 
@@ -14,11 +15,16 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', require('./src/routes/authRoutes'));
 app.use('/api/interview', require('./src/routes/apiRoutes'));
 
 app.use(errorHandler);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
